57039780681ee4da7f64d3304a6184642be9d2d7
Tanner Ellen <tanner@seedcode.com>
2023-03-26 17:45:50 +0000

    Improve colorscheme for readability and portability

--- nvim/.config/nvim/lua/core/colorscheme.lua
+++ nvim/.config/nvim/lua/core/colorscheme.lua
@@ -1,3 +1,22 @@
+-- color palette taken from gruvbox material medium intensity:
+-- https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png
+local customColors = {
+	bright_red = "#ea6962",
+	bright_orange = "#e78a4e",
+	bright_yellow = "#d8a657",
+	bright_green = "#a9b665",
+	bright_aqua = "#89b482",
+	bright_purple = "#d3869b",
+	bright_blue = "#7daea3",
+}
+
+local customHighlights = {
+	TelescopeResultsDiffChange = { fg = customColors.bright_yellow, bg = "NONE" },
+	TelescopeResultsDiffAdd = { fg = customColors.bright_green, bg = "NONE" },
+	TelescopeResultsDiffDelete = { fg = customColors.bright_red, bg = "NONE" },
+	TelescopeResultsDiffUntracked = { bg = "NONE" },
+}
+
 require("gruvbox").setup({
 	transparent_mode = true,
 	undercurl = true,
@@ -12,23 +31,8 @@ require("gruvbox").setup({
 	strikethrough = true,
 	inverse = true, -- invert background for search, diffs, statuslines and errors
 	contrast = "soft", -- can be "hard", "soft" or empty string
-	-- color palette taken from gruvbox material medium intensity:
-	-- https://user-images.githubusercontent.com/58662350/213884019-cbcd5f00-5bef-4a37-9139-0570770330b6.png
-	palette_overrides = {
-		bright_red = "#ea6962",
-		bright_orange = "#e78a4e",
-		bright_yellow = "#d8a657",
-		bright_green = "#a9b665",
-		bright_aqua = "#89b482",
-		bright_purple = "#d3869b",
-		bright_blue = "#7daea3",
-	},
-	overrides = {
-		TelescopeResultsDiffChange = { fg = "#d8a657", bg = "NONE" },
-		TelescopeResultsDiffAdd = { fg = "#a9b665", bg = "NONE" },
-		TelescopeResultsDiffDelete = { fg = "#ea6962", bg = "NONE" },
-		TelescopeResultsDiffUntracked = { bg = "NONE" },
-	},
+	palette_overrides = customColors,
+	overrides = customHighlights,
 })
 
 vim.cmd("colorscheme gruvbox") -- run command to set colorscheme name
