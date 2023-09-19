06059ee7c26a82002b64093f64e3f19ad3264aab
Tanner Ellen <tanner@seedcode.com>
2023-09-19 04:54:36 +0000

    Improve formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,9 @@
-local keymaps = require("core.keymaps") -- keymap reference
-
 require("core.options") -- load neovim settings
+
+local keymaps = require("core.keymaps") -- keymap reference
 keymaps.init() -- initialize and load general keymaps
 keymaps.plugins() -- load plugin specific keymaps
+
 require("core.lazy") -- load lazy nvim plugin manager
 require("core.colorscheme") -- load color theme settings
 require("core.autocmds") -- load auto commands
