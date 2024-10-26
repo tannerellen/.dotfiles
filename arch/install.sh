bab896c839fcb0930e63647dcbec723178ff501e
Tanner Ellen <tanner@seedcode.com>
2024-10-25 20:06:22 -0700

    add noconrim for all arch installs

--- arch/install.sh
+++ arch/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure the basics are installed to continue running this script
-sudo pacman -S --needed kitty git neovim vim stow zip unzip base-devel
+sudo pacman -S --needed kitty git neovim vim stow zip unzip base-devel --noconfirm
 
 # Clone dotfiles
 cd ~
@@ -18,7 +18,7 @@ cd ~/.dotfiles/arch/system
 stow --target=/ *
 
 # Add aur helpers
-sudo pacman -S --needed base-devel git
+sudo pacman -S --needed base-devel git --noconfirm
 cd ~
 git clone https://aur.archlinux.org/paru.git
 cd paru
@@ -29,7 +29,7 @@ mkdir ~/Pictures
 
 # Install ssh
 mkdir ~/.ssh/
-sudo pacman -S openssh
+sudo pacman -S openssh --noconfirm
 
 # Add user to input group so keyboard status works in waybar
 sudo usermod -aG input $(whoami)
@@ -41,7 +41,7 @@ sudo usermod -aG input $(whoami)
 paru -S mkinitcpio-firmware
 
 # Important utilities
-sudo pacman -S man-db starship dosfstools mtools fzf ripgrep
+sudo pacman -S man-db starship dosfstools mtools fzf ripgrep jq fastfetch bottom btop htop --noconfirm
 # When using stow becuase everything is in a subdirectory you must use the --target flag
 # cd ~/.dotfiles/arch
 # stow --target=$HOME
@@ -54,32 +54,40 @@ sudo pacman -S xdg-desktop-portal-gtk
 
 # Authentication agent
 # https://wiki.archlinux.org/title/Polkit
-sudo pacman -S polkit-gnome
+sudo pacman -S polkit-gnome --noconfirm
+
 
 # QT
-sudo pacman -S qt5-wayland qt6-wayland
+sudo pacman -S qt5-wayland qt6-wayland --noconfirm
+
 
 # GTK2 for some apps (like dadroit)
-sudo pacman -S gtk2
+sudo pacman -S gtk2 --noconfirm
+
 
 # Fonts
 paru -S ttf-ubuntu-font-family ttf-firacode-nerd ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd noto-fonts-emoji noto-fonts-cjk noto-fonts-extra noto-fonts nerd-fonts-noto-sans-mono
 
 # Theme
-sudo pacman -S breeze breeze5 breeze-gtk breeze-icons gnome-themes-extra nwg-look qt5ct qt6ct
+sudo pacman -S breeze breeze5 breeze-gtk breeze-icons gnome-themes-extra nwg-look qt5ct qt6ct --noconfirm
+
 
 # Xsettings for xwayland apps used to handle dpi settings for proper sizing
-sudo pacman -S xorg-xrdb
+sudo pacman -S xorg-xrdb --noconfirm
+
 
 # Screen capture
-sudo pacman -S grim slurp
+sudo pacman -S grim slurp --noconfirm
+
 paru -S satty-git # using git version becuase it fixes a bug with copy as of 2024-09-10
 
 # Notification daemon
-sudo pacman -S swaync
+sudo pacman -S swaync --noconfirm
+
 
 # Display manager
-sudo pacman -S sddm sddm-kcm --needed qt6-5compat qt6-declarative qt6-svg
+sudo pacman -S sddm sddm-kcm --needed qt6-5compat qt6-declarative qt6-svg --noconfirm
+
 sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme/
 sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
 sudo sed -i 's/Current=/Current=sddm-astronaut-theme/' /usr/lib/sddm/sddm.conf.d/default.conf
@@ -87,7 +95,8 @@ sudo systemctl enable sddm.service
 
 # Audio
 # sudo pacman -S pipewire wireplumber # Should be installed from arch-install
-sudo pacman -S pavucontrol easyeffects pamixer wob --needed pipewire wireplumber
+sudo pacman -S pavucontrol easyeffects pamixer wob --needed pipewire wireplumber --noconfirm
+
 paru -S cava
 
 # Create listener file for wob volume indicator
@@ -97,7 +106,8 @@ mkfifo /tmp/wobpipe
 # https://wiki.archlinux.org/title/Vulkan
 # https://wiki.archlinux.org/title/OpenGL
 # https://wiki.archlinux.org/title/AMDGPU
-sudo pacman -S wlr-randr vulkan-icd-loader lib32-vulkan-icd-loader vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa libva-mesa-driver
+sudo pacman -S wlr-randr vulkan-icd-loader lib32-vulkan-icd-loader vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa libva-mesa-driver --noconfirm
+
 
 # AMD GPU Overclocking
 paru -S lact
@@ -107,7 +117,7 @@ sudo systemctl enable --now lactd
 # sudo pacman -S pipewire-libcamera # Hyprland and wlr desktop portals currently don't support camera capture so don't install until they do 
 
 # Bluetooth
-sudo pacman -S bluez bluez-utils blueman
+sudo pacman -S bluez bluez-utils blueman --noconfirm
 sudo systemctl enable --now bluetooth.service
 
 # Edit bluetooth settings to allow for bluetooth controllers
@@ -120,7 +130,8 @@ paru -S waybar-cava
 paru -S fuzzel
 
 # File manager
-sudo pacman -S thunar gvfs
+sudo pacman -S thunar gvfs --noconfirm
+
 
 # wlogout
 paru -S wlogout
@@ -133,66 +144,60 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 source ~/.bashrc
 nvm install v20.17.0
 
-# Resource usage tools
-sudo pacman -S bottom btop htop
-
 # Image viewers
-sudo pacman -S imv
+sudo pacman -S imv --noconfirm
 paru -S coreimage
 
 # Video / screencasts
 paru -S wf-recorder
 
 # RGB control
-sudo pacman -S openrgb
-
-# Neofetch alternative since it is no longer maintained
-sudo pacman -S fastfetch
-
-# jq for processing json
-sudo pacman -S jq
+sudo pacman -S openrgb --noconfirm
 
 # Keyring / Passwords
 # https://wiki.archlinux.org/title/GNOME/Keyring
-sudo pacman -S gnome-keyring libsecret seahorse
+sudo pacman -S gnome-keyring libsecret seahorse --noconfirm
 systemctl --user enable --now gcr-ssh-agent.socket
 
 # Fuse required for app images
-sudo pacman -S fuse2
+sudo pacman -S fuse2 --noconfirm
+
 
 # Backups
-sudo pacman -S timeshift xorg-xhost
+sudo pacman -S timeshift xorg-xhost --noconfirm
+
 
 # Network discover mdns
-sudo pacman -S avahi nss-mdns
+sudo pacman -S avahi nss-mdns --noconfirm
+
 sudo systemctl enable --now avahi-daemon.service
 sudo sed -i 's/mymachines /mymachines mdns_minimal [NOTFOUND=return] /' /etc/nsswitch.conf
 
 # Printing
 # https://wiki.archlinux.org/title/CUPS
-sudo pacman -S cups ghostscript
+sudo pacman -S cups ghostscript --noconfirm
 sudo systemctl enable --now cups.service
 
 # Sensors and fans
-sudo pacman -S lm_sensors
+sudo pacman -S lm_sensors --noconfirm
 # sudo sensors-detect --auto
 
 # i2c / rgb
-sudo pacman -S i2c-tools
+sudo pacman -S i2c-tools --noconfirm
 # enable i2c bus for rgb
 # echo 'i2c-dev' | sudo tee -a /etc/modules-load.d/i2c-dev.conf
 
 # Software to use loopback video device to share screen as second camera
 # https://www.math.cmu.edu/~gautam/sj/blog/20220326-zoom-wayland.html
-sudo pacman -S v4l2loopback-utils v4l2loopback-dkms linux-headers
+sudo pacman -S v4l2loopback-utils v4l2loopback-dkms linux-headers --noconfirm
 # Launch obs and click "Start Virtual Camera"
 
 ##### User apps #####
-sudo pacman -S firefox lazygit yazi imagemagick gtk-vnc transmission-gtk p7zip gamescope syncthing gparted
+sudo pacman -S firefox lazygit yazi imagemagick gtk-vnc transmission-gtk p7zip gamescope syncthing gparted steam --noconfirm
 paru -S 1password-beta wlrobs webapp-manager kalc wayvnc parsec balena-etcher amdgpu_top-bin wlvncc-git
 
 # Install flatpak
-sudo pacman -S flatpak
+sudo pacman -S flatpak --noconfirm
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --user flatseal
@@ -221,20 +226,6 @@ flatpak install --user com.usebottles.bottles
 # Heroic game launcher
 flatpak install flathub com.heroicgameslauncher.hgl
 
-# AWS CLI
-cd ~
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm awscliv2.zip
-rm -rf aws
-# Authenticat with:
-aws configure
-
-##### Games and emulation #####
-# https://wiki.archlinux.org/title/Steam
-sudo pacman -S steam
-
 # Ryujinx AppImage
 # https://github.com/ryujinx-mirror/ryujinx
 cd ~
@@ -253,3 +244,14 @@ sudo sed -i 's/Exec=/Exec=env GDK_SCALE=1 /' ~/.local/share/flatpak/exports/shar
 
 # Update man page caches
 sudo mandb
+
+# AWS CLI
+cd ~
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -rf aws
+# Authenticat with:
+# aws configure
+
