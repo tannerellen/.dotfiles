a363853389b55aef9148549428a6b02f93d03f3b
Tanner Ellen <tanner@seedcode.com>
2025-05-22 20:08:30 -0700

    adds dimensions to screenshot region selection

--- arch/config/hypr/.config/hypr/scripts/screenshot.sh
+++ arch/config/hypr/.config/hypr/scripts/screenshot.sh
@@ -29,7 +29,7 @@ filePath="$directory/$filePrefix-$(date '+%Y%m%d-%H:%M:%S').$format"
 echo $filePath
 
 if [[ $mode == "region" ]]; then
-	grim -g "$(slurp)" - | satty --filename - --fullscreen --early-exit --copy-command "wl-copy --type image/png" --output-filename "$filePath"
+	grim -g "$(slurp -d)" - | satty --filename - --fullscreen --early-exit --copy-command "wl-copy --type image/png" --output-filename "$filePath"
 elif [[ $mode == "window" ]]; then
 	grim -g "$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" - | satty --filename - --fullscreen --early-exit --copy-command "wl-copy --type image/png" --output-filename "$filePath"
 else
