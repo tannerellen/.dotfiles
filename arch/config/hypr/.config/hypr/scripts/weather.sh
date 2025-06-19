762a13b71908930bc83d81c76eb862943679aecc
Tanner Ellen <tanner@seedcode.com>
2025-06-18 23:08:20 -0700

    enables weather again since internet seems to have correct location now

--- arch/config/hypr/.config/hypr/scripts/weather.sh
+++ arch/config/hypr/.config/hypr/scripts/weather.sh
@@ -1,55 +1,30 @@
 #!/bin/bash
 
-# Get the current location based on external IP address.
-# ifconfig.me gets external IP then ipapi gets the location from that
-# location=$(curl -s "https://ipapi.co/$(curl -s ifconfig.me)/json/" | jq -r '"\(.city)+\(.region_code)"' | sed 's/ /+/g')
-
-location="Lake+Stevens+WA"
-
-# Get the weather from wttr formatted to just icon and temp
-weather=$(curl -s wttr.in/"$location"?format=1)
-
+# # Get the current location based on external ip address.
+# # ifconfig.me get's external ip then ipapi gets the location from that
+location=$(curl -s "https://ipapi.co/$(curl -s ifconfig.me)/json/" | jq -r '"\(.city)+\(.region_code)"' | sed 's/ /+/g')
+#
+# # Get the weather from wttr formatted to just icon and temp
+weather=$(curl -s wttr.in/"$location"?format="%c+%t")
+#
 if [[ "$weather" == "Unknown"* ]]; then
     echo "?"
-    exit;
+	exit;
 fi
-
-# Remove excess whitespace and other formatting
+#
+# # Convert the temp to fahrenheit remove excess whitespace and other formatting
 output=$(echo "$weather" | \
     sed -e 's/  \+/ /g' \
         -e 's/+//g' \
-        -e 's/°C//g' \
-        -e 's/°F/°/g')  # Optional: If you want to keep the degree symbol
+        -e 's/°C//g' | \
+    awk '{
+        if (match($0, /-?[0-9]+/)) {
+            temp = substr($0, RSTART, RLENGTH);
+            fahrenheit = (temp * 9/5) + 32;
+            fahrenheit = int(fahrenheit + 0.5);
+            gsub(/-?[0-9]+/, fahrenheit "°");
+        }
+        print $0
+    }')
 
 echo "$output"
-
-# #!/bin/bash
-#
-# # Get the current location based on external ip address.
-# # ifconfig.me get's external ip then ipapi gets the location from that
-# location=$(curl -s "https://ipapi.co/$(curl -s ifconfig.me)/json/" | jq -r '"\(.city)+\(.region_code)"' | sed 's/ /+/g')
-#
-# # Get the weather from wttr formatted to just icon and temp
-# weather=$(curl -s wttr.in/"$location"?format="%c+%t")
-#
-# if [[ "$weather" == "Unknown"* ]]; then
-#     echo "?"
-# 	exit;
-# fi
-#
-# # Convert the temp to fahrenheit remove excess whitespace and other formatting
-# output=$(echo "$weather" | \
-#     sed -e 's/  \+/ /g' \
-#         -e 's/+//g' \
-#         -e 's/°C//g' | \
-#     awk '{
-#         if (match($0, /-?[0-9]+/)) {
-#             temp = substr($0, RSTART, RLENGTH);
-#             fahrenheit = (temp * 9/5) + 32;
-#             fahrenheit = int(fahrenheit + 0.5);
-#             gsub(/-?[0-9]+/, fahrenheit "°");
-#         }
-#         print $0
-#     }')
-#
-# echo "$output"
