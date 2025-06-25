a9a4306c82082df1d737445cb10fed0d4f0e686d
Tanner Ellen <tanner@seedcode.com>
2025-06-24 19:48:05 -0700

    updates sleep and wlogout

--- arch/config/hypr/.config/hypr/scripts/sleep.sh
+++ arch/config/hypr/.config/hypr/scripts/sleep.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sleep 0.25 # Very small delay to give screen time to clear before hyprlock appears
 pidof hyprlock || hyprlock --immediate & 
-sleep 0.5
 hyprctl dispatch dpms off &&
+# The following delay will do 2 things...
+# 1. It will allow hyprlock time to render so when waking it's already up.
+# 2. It will allow the monitor to fully sleep before computer sleeps preventing crash that appears to affect the gpu
 sleep 3
 systemctl suspend
