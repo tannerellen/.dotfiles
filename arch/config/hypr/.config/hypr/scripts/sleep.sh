b410de7bed93a3ff5920da2171880383f4e3dbb8
Tanner Ellen <tanner@seedcode.com>
2025-06-28 01:15:34 -0700

    updates sleep script to do suspend to hibernate on laptops

--- arch/config/hypr/.config/hypr/scripts/sleep.sh
+++ arch/config/hypr/.config/hypr/scripts/sleep.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# If running on a laptop suspend then hibernate
+if ls /sys/class/power_supply/BAT* 1> /dev/null 2>&1 || ls /proc/acpi/battery/BAT* 1> /dev/null 2>&1; then
+	systemctl suspend-then-hibernate
+	exit 0;
+fi
+
+# If on a desktop let's do regular suspend with fixes
 sleep 0.25 # Very small delay to give screen time to clear before hyprlock appears
 pidof hyprlock || hyprlock --immediate & 
 sleep 0.25 # Very small delay to give hyprlock time to render to prevent flash on wake
