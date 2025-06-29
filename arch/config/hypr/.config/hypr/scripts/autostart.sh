56dc017a16eff80254f05c138740ed1861f85975
Tanner Ellen <tanner@seedcode.com>
2025-06-28 21:00:10 -0700

    updates x11 scaling

--- arch/config/hypr/.config/hypr/scripts/autostart.sh
+++ arch/config/hypr/.config/hypr/scripts/autostart.sh
@@ -11,7 +11,7 @@ sleep 1
 # Launch 1password
 # Force running in xwayland as copying to the clipboard wasn't working in wayland
 # Scale is set otherwise it opens large on a hdpi display
-ELECTRON_OZONE_PLATFORM_HINT=x11 GDK_SCALE=1 /usr/bin/1password --silent "$@" &
+ELECTRON_OZONE_PLATFORM_HINT=x11 /usr/bin/1password --silent "$@" &
 # Open using the standard wayland method - Currently disabled
 # /usr/bin/1password --silent "$@" &
 #
