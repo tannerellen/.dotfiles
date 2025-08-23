f17ebd288ed69f356fea52a56ec3f4333388de74
Tanner Ellen <tanner@seedcode.com>
2025-08-23 09:59:01 -0700

    changes 1password to wayland from xwayland

--- arch/config/hypr/.config/hypr/scripts/autostart.sh
+++ arch/config/hypr/.config/hypr/scripts/autostart.sh
@@ -11,9 +11,9 @@ sleep 1
 # Launch 1password
 # Force running in xwayland as copying to the clipboard wasn't working in wayland
 # Scale is set otherwise it opens large on a hdpi display
-ELECTRON_OZONE_PLATFORM_HINT=x11 /usr/bin/1password --silent "$@" &
+# ELECTRON_OZONE_PLATFORM_HINT=x11 /usr/bin/1password --silent "$@" &
 # Open using the standard wayland method - Currently disabled
-# /usr/bin/1password --silent "$@" &
+/usr/bin/1password --silent "$@" &
 #
 # Start wob for volume indicator
 rm -f /tmp/wobpipe
