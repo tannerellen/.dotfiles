18ed868b5cfee41a9e368213f41d4ee357dc9410
Tanner Ellen <tanner@seedcode.com>
2025-10-24 22:05:18 -0700

    fix: 1password modal too small

--- arch/config/hypr/.config/hypr/scripts/autostart.sh
+++ arch/config/hypr/.config/hypr/scripts/autostart.sh
@@ -6,19 +6,20 @@ sleep 2
 # Launch bluetooth manager - Running after delay to give time for daemon to start first
 blueman-applet &
 
-sleep 1
 
-# Launch 1password
-# Force running in xwayland as copying to the clipboard wasn't working in wayland
-# Scale is set otherwise it opens large on a hdpi display
-# ELECTRON_OZONE_PLATFORM_HINT=x11 /usr/bin/1password --silent "$@" &
-# Open using the standard wayland method - Currently disabled
-/usr/bin/1password --silent "$@" &
 #
 # Start wob for volume indicator
 rm -f /tmp/wobpipe
 mkfifo /tmp/wobpipe
 tail -f /tmp/wobpipe | wob &
+
+sleep 1
+
+# Launch 1password
+# Force running in xwayland as sometimes unlock window opens too small and cut off
+ELECTRON_OZONE_PLATFORM_HINT=x11 /usr/bin/1password --silent "$@" &
+# Open using the standard wayland method - Currently disabled
+# /usr/bin/1password --silent "$@" &
 
 # Execute applications on specific workspaces
 hyprctl dispatch exec "[workspace 1 silent] firefox"
