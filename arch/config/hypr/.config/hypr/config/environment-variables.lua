eba102f3e6df407b03b0785877973e6c88080a83
Tanner Ellen <tanner@seedcode.com>
2026-08-17 19:01:07 -0700

    update qt platform var

--- arch/config/hypr/.config/hypr/config/environment-variables.lua
+++ arch/config/hypr/.config/hypr/config/environment-variables.lua
@@ -16,7 +16,7 @@ hl.env("GDK_SCALE", "2")
 -- QT
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 -- env = QT_QPA_PLATFORMTHEME,gtk3
-hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct:qt6ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 
