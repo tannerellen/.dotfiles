bc34e0df2db7b7bb7fe261beeace860d91b968fc
Tanner Ellen <tanner@seedcode.com>
2026-09-07 22:36:09 -0700

    updates nvim script to execute faster

--- arch/scripts/scripts/.scripts/nvim.sh
+++ arch/scripts/scripts/.scripts/nvim.sh
@@ -2,5 +2,7 @@
 
 # Script used to launch neovim from outside terminal. This will ensure that nodejs
 # installed with nvim is properly referenced so tooling continues to work
-source "$HOME/.nvm/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+NODE_BIN=$(ls -d "$NVM_DIR"/versions/node/*/bin 2>/dev/null | sort -V | tail -1)
+export PATH="$NODE_BIN:$PATH"
 exec nvim "$@"
